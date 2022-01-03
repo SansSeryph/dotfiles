ae8dc6e5ef0119e385f1159adec721c6b8b0d86a
SansSeryph <sansseryph@gmail.com>
2022-01-02 23:27:54 -0800

    Replace double quotes with single

--- dotfiles/config/nvim/lua/errors.lua
+++ dotfiles/config/nvim/lua/errors.lua
@@ -2,10 +2,10 @@ local M = {}
 
 function M.configure(use, keymap)
   use {
-    "folke/trouble.nvim",
-    requires = "kyazdani42/nvim-web-devicons",
+    'folke/trouble.nvim',
+    requires = 'kyazdani42/nvim-web-devicons',
   }
-  require("trouble").setup({})
+  require('trouble').setup({})
 
   -- x namespace: tools for vim itself
   keymap('', '<C-e>', '<Cmd>TroubleToggle<CR>', silent)
