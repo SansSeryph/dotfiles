1e6b0b9ffad05ab8d6ba3ee452d494aee96ce4ad
SansSeryph <sansseryph@gmail.com>
2022-01-02 23:28:43 -0800

    Add Trouble keymaps

--- dotfiles/config/nvim/lua/errors.lua
+++ dotfiles/config/nvim/lua/errors.lua
@@ -7,8 +7,17 @@ function M.configure(use, keymap)
   }
   require('trouble').setup({})
 
-  -- x namespace: tools for vim itself
-  keymap('', '<C-e>', '<Cmd>TroubleToggle<CR>', silent)
+  local opts = { silent = true, noremap = true }
+
+  -- remaps
+  keymap('n', 'gR', '<Cmd>Trouble lsp_references<CR>', opts)
+
+  -- x namespace: trouble
+  keymap('n', '<leader>xx', '<Cmd>TroubleToggle<CR>', opts)
+  keymap('n', '<leader>xw', '<Cmd>Trouble workspace_diagnostics<CR>', opts)
+  keymap('n', '<leader>xd', '<Cmd>Trouble document_diagnostics<CR>', opts)
+  keymap('n', '<leader>xl', '<Cmd>Trouble loclist<CR>', opts)
+  keymap('n', '<leader>xq', '<Cmd>Trouble quickfix<CR>', opts)
 end
 
 return M
