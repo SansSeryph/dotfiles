7677ded108a9c8c758ede5559d90cee73ae2a83c
SansSeryph <sansseryph@gmail.com>
2022-01-02 23:29:54 -0800

    Add lspconfig settings

--- dotfiles/config/nvim/lua/language_servers.lua
+++ dotfiles/config/nvim/lua/language_servers.lua
@@ -65,6 +65,19 @@ function M.configure(use)
       flags = { debounce_text_changes = 150 }
     }
   end
+
+  vim.diagnostic.config({
+    update_in_insert = true,
+    severity_sort = true,
+    virtual_text = { source = 'always' },
+    float = { source = 'always' },
+  })
+
+  local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+  for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+  end
 end
 
 return M
