1d748f68dd21641b335f631b0164a835ae85e7a6
SansSeryph <sansseryph@gmail.com>
2022-01-17 14:00:13 -0800

    Finder updates
    - Move t namespace to now be f
    - Remove grep_string shortcut
    - Move telescope dependencies to be inside the telescope requires

--- dotfiles/config/nvim/lua/finder.lua
+++ dotfiles/config/nvim/lua/finder.lua
@@ -1,14 +1,15 @@
 local M = {}
 
 function M.configure(use)
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-  use 'kyazdani42/nvim-web-devicons'
-  use 'nvim-lua/plenary.nvim'
-  use 'xiyaowong/telescope-emoji.nvim'
-  use 'nvim-telescope/telescope-packer.nvim'
   use {
     'nvim-telescope/telescope.nvim',
-    requires = { {'nvim-lua/plenary.nvim'} }
+    requires = {
+      { 'kyazdani42/nvim-web-devicons' },
+      { 'nvim-lua/plenary.nvim' },
+      { 'nvim-telescope/telescope-packer.nvim' },
+      { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+      { 'xiyaowong/telescope-emoji.nvim' },
+    }
   }
 
   require('nvim-web-devicons').setup({})
@@ -41,16 +42,18 @@ function M.configure(use)
   -- ctrl maps
   keymap('', '<C-f>', '<Cmd>Telescope find_files<CR>', noremap)
   keymap('', '<C-g>', '<Cmd>Telescope live_grep<CR>', noremap)
-  keymap('', '<C-S-G>', '<Cmd>Telescope grep_string<CR>', noremap)
   keymap('', '<C-b>', '<Cmd>Telescope buffers<CR>', noremap)
 
   -- remaps
   keymap('n', 'gd', '<Cmd>Telescope lsp_definitions<CR>', {})
 
-  -- t namespace: Telescope
-  keymap('n', '<leader>tt', '<Cmd>Telescope tags<CR>', noremap)
-  keymap('n', '<leader>th', '<Cmd>Telescope help_tags<CR>', noremap)
-  keymap('n', '<leader>te', '<Cmd>Telescope emoji<CR>', noremap)
+  -- f namespace: Telescope
+  keymap('n', '<leader>ff', '<Cmd>Telescope find_files<CR>', noremap)
+  keymap('n', '<leader>fg', '<Cmd>Telescope live_grep<CR>', noremap)
+  keymap('n', '<leader>fb', '<Cmd>Telescope buffers<CR>', noremap)
+  keymap('n', '<leader>ft', '<Cmd>Telescope tags<CR>', noremap)
+  keymap('n', '<leader>fh', '<Cmd>Telescope help_tags<CR>', noremap)
+  keymap('n', '<leader>fe', '<Cmd>Telescope emoji<CR>', noremap)
 end
 
 return M
