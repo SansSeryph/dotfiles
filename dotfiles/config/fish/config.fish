dc724e37b5db63a20a119b49e2ceffdf2b6ce3fa
SansSeryph <sansseryph@gmail.com>
2022-10-24 11:26:03 -0700

    Account for m1 mac laptops

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,17 +1,12 @@
 if status is-interactive
   set --local os (uname -s)
 
-  if test $os = "Darwin"
-    source /usr/local/opt/asdf/libexec/asdf.fish
-  else if test $os = "Linux"
+  if [ $os = "Darwin" ]
+    set -x BREW_PREFIX (brew --prefix)
+    eval ($BREW_PREFIX/bin/brew shellenv)
+    source $BREW_PREFIX/opt/asdf/libexec/asdf.fish
+  else if [ $os = "Linux" ]
     source /opt/asdf-vm/asdf.fish
-  else
-    echo "OS type not listed"
-  end
-
-  set --local bin_dir ~/Workspace/bin
-  if test -d $bin_dir
-    set --append PATH $bin_dir
   end
 end
 
